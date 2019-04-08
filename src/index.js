73c407c513cff1f65705f6f18f10c6a7b197c3fa
Adam <adampugh@zoho.com>
2019-04-08 16:35:13 +0100

    Added notes

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ import * as serviceWorker from './serviceWorker';
 // build api for card images + data
 
 
-
-
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
